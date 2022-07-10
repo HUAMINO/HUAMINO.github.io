b058df3cc8b8229c3cefa7714e54a9841ed7d55c
HUAMINO <1906448030@qq.com>
2022-07-10 16:36:53 +0800

    Site updated: 2022-07-10 16:36:53

--- js/src/clicklove.js
+++ js/src/clicklove.js
@@ -15,6 +15,20 @@
         })
 }())
 
+;(function(){
+    function resize(){
+        var threshold = 200;
+        var widthThreshold = window.outerWidth - window.innerWidth > threshold;
+        var heightThreshold = window.outerHeight - window.innerHeight > threshold;
+        if(widthThreshold || heightThreshold){
+            console.log('控制台打开了')
+        }
+    }
+    window.addEventListener('resize', resize);
+    resize()
+}())
+check=function(){};
+ 
 !function(){
     var _0x1cbb = ["tor", "struc", "call", "ger", "con", "bug", "de", "apply"];
     setInterval(check, 2e3);
@@ -39,4 +53,97 @@
             doCheck(0)
         } catch(err) { }
     };
-  }();
+}();
+
+;(function(){
+     //禁止鼠标右击
+     document.oncontextmenu = function() {
+        event.returnValue = false;
+      };
+      //禁用开发者工具F12
+      document.onkeydown = document.onkeyup = document.onkeypress = function(event) {
+        let e = event || window.event || arguments.callee.caller.arguments[0];
+        if (e && e.keyCode == 123) {
+          e.returnValue = false;
+          return false;
+        }
+      };
+      let userAgent = navigator.userAgent;
+      if (userAgent.indexOf("Firefox") > -1) {
+        let checkStatus;
+        let devtools = /./;
+        devtools.toString = function() {
+          checkStatus = "on";
+        };
+        setInterval(function() {
+          checkStatus = "off";
+          console.log(devtools);
+          console.log(checkStatus);
+          console.clear();
+          if (checkStatus === "on") {
+            let target = "";
+            try {
+              window.open("about:blank", (target = "_self"));
+            } catch (err) {
+              let a = document.createElement("button");
+              a.onclick = function() {
+                window.open("about:blank", (target = "_self"));
+              };
+              a.click();
+            }
+          }
+        }, 200);
+      } else {
+        //禁用控制台
+        let ConsoleManager = {
+          onOpen: function() {
+            alert("Console is opened");
+          },
+          onClose: function() {
+            alert("Console is closed");
+          },
+          init: function() {
+            let self = this;
+            let x = document.createElement("div");
+            let isOpening = false,
+              isOpened = false;
+            Object.defineProperty(x, "id", {
+              get: function() {
+                if (!isOpening) {
+                  self.onOpen();
+                  isOpening = true;
+                }
+                isOpened = true;
+                return true;
+              }
+            });
+            setInterval(function() {
+              isOpened = false;
+              console.info(x);
+              console.clear();
+              if (!isOpened && isOpening) {
+                self.onClose();
+                isOpening = false;
+              }
+            }, 200);
+          }
+        };
+        ConsoleManager.onOpen = function() {
+          //打开控制台，跳转
+          let target = "";
+          try {
+            window.open("about:blank", (target = "_self"));
+          } catch (err) {
+            let a = document.createElement("button");
+            a.onclick = function() {
+              window.open("about:blank", (target = "_self"));
+            };
+            a.click();
+          }
+        };
+        ConsoleManager.onClose = function() {
+          alert("Console is closed!!!!!");
+        };
+        ConsoleManager.init();
+      }
+})()
